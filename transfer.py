c72505f3010847e0cad1ed1ac309acd1a81d6c36
applejenny66 <applejenny66@gmail.com>
2019-11-10 00:03:46 +0800

    .

--- transfer.py
+++ transfer.py
@@ -2,8 +2,63 @@
 
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
-img = cv2.imread("pikachu.jpg")
-cv2.imshow("pika", img)
+
+img = cv2.imread("pikachu.jpg", 0)
+#plt.hist(img.ravel(),256,[0,256]); plt.show()
+#hist = cv2.calcHist([img],[0],None,[256],[0,256])
+#print (type(hist))
+#print (hist.shape) (256, 1)
+
+#dev_hist = []
+
+#for i in range(0, 255):
+#    tmp_dev = abs(hist[i+1] - hist[i])
+#    dev_hist.append(tmp_dev)
+
+#max_pixel = max(dev_hist)
+#max_index = dev_hist.index(max_pixel)
+
+#dev_hist.remove(max_pixel)
+#second_max_pixel = max(dev_hist)
+#second_index = dev_hist.index(second_max_pixel)
+#min_pixel = min(dev_hist)
+#print (max_pixel, max_index, second_max_pixel, second_index)
+#img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+
+kernel = np.ones((10,10),np.float32)/100
+dst = cv2.filter2D(img,-1,kernel)
+#cv2.imshow('dst', dst)
+img_gray = cv2.cvtColor(dst,cv2.COLOR_BGR2GRAY)
+ret2,thresh2 = cv2.threshold(img_gray,100,255,0) #127
+print (type(thresh2))
+print (thresh2.shape)
+cv2.imshow('thresh2', thresh2)
+
+Z = img.reshape((-1,3))
+Z = np.float32(Z)
+
+# define criteria, number of clusters(K) and apply kmeans()
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+K = 3
+ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+center = np.uint8(center)
+res = center[label.flatten()]
+res2 = res.reshape((img.shape))
+#print (type(ret), type(label), type(center))
+#cv2.imshow('thresh2', res2)
+
+
+
+# Now convert back into uint8, and make original image
+
+
+contours, hierarchy = cv2.findContours(thresh2,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+img_contour = cv2.drawContours(img, contours, -1, (0,255,0), 3)
+
+cv2.imshow('contour',img_contour)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
---
+++ transfer_test.py
@@ -0,0 +1,74 @@
+# transfer.py
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def readimg(name):
+    img = cv2.imread(name)
+    return (img)
+
+def grayimg(img, name):
+    img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    cv2.imwrite(name, img_gray)
+    return (img_gray)
+
+def showdistribution(img):
+    plt.hist(img.ravel(),256,[0,256])
+    plt.savefig("hist.png")
+    #plt.show()
+    #hist = cv2.calcHist([img],[0],None,[256],[0,256])
+
+def blurredimg(img, kernel_size=10):    
+    kernel = np.ones((kernel_size,kernel_size),np.float32)/(kernel_size*kernel_size)
+    dst = cv2.filter2D(img,-1,kernel)
+    cv2.imwrite('blurred.png', dst)
+    #cv2.imshow('dst', dst)
+    return (dst)
+
+def threshold(img, x1, x2):
+    ret2, thresh = cv2.threshold(img,x1,x2,0) #127
+    cv2.imwrite("thrshold.png", thresh)
+    return (ret2, thresh)
+
+def kmeans(img, K = 3):
+    Z = img.reshape((-1,3))
+    Z = np.float32(Z)
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    res2 = res.reshape((img.shape))
+    return (res2)
+
+
+def drawContours(draw_img, thresh_img):
+    contours, hierarchy = cv2.findContours(thresh_img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    img_contour = cv2.drawContours(draw_img, contours, -1, (0,255,0), 3)
+    cv2.imwrite('contour.png', img_contour)
+    #cv2.imshow('contour',img_contour)
+    return (img_contour)
+
+def main():
+    img = readimg('pikachu.jpg')
+    img_gray = grayimg(img, 'img_gray.png')
+    showdistribution(img_gray)
+    blurred_img = blurredimg(img, 10)
+    k_img = kmeans(blurred_img, K = 3)
+    k_gray_img = grayimg(k_img, 'k_img.png')
+    blurred_gray_img = cv2.cvtColor(blurred_img, cv2.COLOR_BGR2GRAY)
+    #ret2, thresh = threshold(blurred_gray_img,150,255) #127
+    ret2, thresh = threshold(k_gray_img,140,255)
+    img_contour = drawContours(k_img, thresh)
+    
+
+
+
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+
+
+if __name__ == "__main__":
+    main()
