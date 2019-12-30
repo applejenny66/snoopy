4cdbcdc65ebbdc529a2e6921e874eefcb1b4b76d
applejenny66 <applejenny66@gmail.com>
2019-12-30 14:37:55 +0800

    .

--- writing/test.py
+++ writing/test.py
@@ -1,4 +1,26 @@
 import pyttsx
-engine=pyttsx.init()
-engine.say('Sally sells seashells by the seashore.')
+#import pyttsx3
+"""
+engine = pyttsx.init()
+engine.say('hello world')
+engine.say('start')
 engine.runAndWait()
+engine.say('start')
+engine.endLoop()
+
+engine.say('1')
+engine.runAndWait()
+engine.endLoop()
+"""
+
+
+def Txt2Voice(text):
+    
+    engine = pyttsx.init()
+    engine.say(text)
+    engine.runAndWait()
+
+text='start'
+Txt2Voice(text)
+text2 = '1'
+Txt2Voice(text2)
